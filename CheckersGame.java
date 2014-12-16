6ed22e676508139efa2f4807a574edff969120ee
TheChyz <thechyz@hotmail.com>
2014-12-15 22:37:52 -0800

    Create CheckersGame.java

---
+++ CheckersGame.java
@@ -0,0 +1,50 @@
+/**
+ * Uses the rules of checkers to allow two human players to play against each other
+ * @author Michael Chyziak (mchyziak@sfu.ca)
+ * @version 1
+ * @since 15-12-2014 (DD-MM-YYYY)
+ */
+public class CheckersGame {
+
+	Board board;
+	
+	/**
+	 * Creates a new checkers Game and plays it
+	 */
+	public CheckersGame() {
+		board = new Board();
+		runGame();
+	}
+	
+	
+	/**
+	 * Creates 2 players and the players take turns until the game is over
+	 */
+	private void runGame() {
+		Player p1 = new Player(Piece.pieceColor.White);
+		Player p2 = new Player(Piece.pieceColor.Black);
+		while (!isGameOver()) {
+			p1.turn();
+			if (!isGameOver()) {
+				p2.turn();
+			}
+			else {
+				break;
+			}
+		}
+	}
+	
+	/**
+	 * Checks if the game is over. This happens when a player has no more pieces left
+	 * @return true if the game is over, false otherwise
+	 */
+	private boolean isGameOver() {
+		if (board.getBlackPieces().size() == 0 || board.getWhitePieces().size() == 0) {
+			return true;
+		}
+		return false;
+		
+		// TODO include cases for stalemate and ties and such stuff.
+	}
+	
+}
