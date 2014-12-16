b7375daba1ca3aaa05a74ee3c192cb9f7c71a7e1
TheChyz <thechyz@hotmail.com>
2014-12-15 22:37:15 -0800

    Create MoveList.java

---
+++ MoveList.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+/**
+ * Stores any possible moves that a piece can make
+ * @author Michael Chyziak (mchyziak@sfu.ca)
+ * @version 1
+ * @since 15-12-2014 (DD-MM-YYYY)
+ */
+public class MoveList {
+
+	/**
+	 * Do nothing
+	 */
+	public MoveList() {
+		// TODO
+	}
+
+	
+	/**
+	 * 
+	 * @param rowPosition
+	 * @param columnPosition
+	 * @param whitePieces
+	 * @param blackPieces
+	 * @return
+	 */
+	public boolean isValidMove(int rowPosition, int columnPosition, ArrayList<Piece> whitePieces, ArrayList<Piece> blackPieces) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	/**
+	 * Returns the total number of valid moves a player can make
+	 * @return the integer value of the total number of moves that are possible
+	 */
+	public int getNumberOfValidMoves() {
+		// TODO
+		return 0;
+	}
+
+
+	/**
+	 * Creates the move list that are possible
+	 */
+	public void createMoveList() {
+		// TODO Auto-generated method stub
+		
+	}
+}
