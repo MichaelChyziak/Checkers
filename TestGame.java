c298824dc06838da5dff2dc3bdca522df4ecb9e1
TheChyz <thechyz@hotmail.com>
2014-12-15 22:37:35 -0800

    Create TestGame.java

---
+++ TestGame.java
@@ -0,0 +1,16 @@
+/**
+ * Testing of the Checkers Game
+ * @author Michael Chyziak (mchyziak@sfu.ca)
+ * @version 1
+ * @since 15-12-2014 (DD-MM-YYYY)
+ */
+public class TestGame {
+
+	/**
+	 * Runs the checkers game
+	 * @param args 
+	 */
+	public static void main(String[] args) {
+		new CheckersGame();
+	}
+}
